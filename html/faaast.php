d5df2121183df86079ec438bf5f7eb57fbd18ef1
TheodorosPloumis <me@theodorosploumis.com>
2017-11-04 04:17:40 +0200

    Validate command with fodler size

--- html/faaast.php
+++ html/faaast.php
@@ -131,8 +131,8 @@ if (isset($_GET['cmd'])) {
     }
 
     $cmd_main = $cmd . " > /error/command.log 2>&1";
-    //$cmd_validate = "du -shb ".$folder." | awk \'{print $1}\'";
-    $cmd_exit = "$(if [ $? != 0 ]; then exit; fi)";
+    $cmd_exit = "if [ $(du -shb /home | awk \'{print $1}\') lt 8000 ]; then exit; fi";
+    //$cmd_exit = "if [ $? != 0 ]; then exit; fi";
     $cmd_cd = " cd " . $folder;
     $cmd_chown_home = " chown -R www-data:www-data " . $folder;
 
