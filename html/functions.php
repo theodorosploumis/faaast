3ee8cf01026cbea89015591565525d7fa189b733
TheodorosPloumis <me@theodorosploumis.com>
2017-11-07 19:50:19 +0200

    Add github link

--- html/functions.php
+++ html/functions.php
@@ -131,6 +131,8 @@ function footerMessage() {
     $text .= ' | ';
     $text .= "<a href='/faqs.php'>FAQs</a>";
     $text .= ' | ';
+    $text .= "<a href='https://github.com/theodorosploumis/faaast/'>GitHub</a>";
+    $text .= ' | ';
     $text .= 'Hosted on <a href="https://m.do.co/c/1123d0854c8f">DigitalOcean</a>';
 
     return $text;
