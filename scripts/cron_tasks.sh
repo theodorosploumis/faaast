0ff2f7ad2dc4f089ddab1091f02974aafd5b847a
Theodoros Ploumis <me@theodorosploumis.com>
2018-01-18 22:22:09 +0200

    Use faaast folder

--- scripts/cron_tasks.sh
+++ scripts/cron_tasks.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 # Crontab task
-# * * * * * (sleep 5; /bin/bash /var/www/scripts/cron_tasks.sh)
-chown -R www-data:www-data /var/www/html/builds /var/www/error
+# * * * * * (sleep 5; /bin/bash /var/www/faaast/scripts/cron_tasks.sh)
+chown -R www-data:www-data /var/www/faaast/html/builds /var/www/faaast/error
